e7a733a2a93d011e346577382c22c454411b35a2
Tarunjais23 <164129419+Tarunjais23@users.noreply.github.com>
2024-07-12 12:52:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ USERNAME = environ.get('USERNAME', "https://telegram.me/biisal")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002009145832'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/tjmovies07')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002047519334').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tarun234:tarun234@cluster0.v3n2umt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://tarun234:tarun234@cluster0.v3n2umt.mongodb.net/?retryWrites=true&w=majority")
 
 DATABASE_NAME = environ.get('DATABASE_NAME', "TELEGRAM_BOT_INFO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
